8b063f932eb4070d753b591d043a4240b676311a
isaac hershenson <ihershenson@hmc.edu>
2024-07-21 21:00:17 -0700

    updated inputs

--- langchain/main-hackercup.py
+++ langchain/main-hackercup.py
@@ -4,55 +4,66 @@ from langchain_openai import ChatOpenAI
 
 from hackercup_graph import get_graph_for_problem
 from utils.utils import get_problemset
+import argparse
+import os
 
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
 
-'''
-In order to run this code on a problem other than the Dim Sum Delivery problem, please edit the code below
+if __name__=="__main__":
 
-You will need to change the contents of the assets/practice_problem directory in order for it to work
-'''
-(problem_statement, input) = get_problemset()
-problem_content = problem_statement["contents"]
+    parser = argparse.ArgumentParser(description="Solve Hackercup coding challenge.")
+    parser.add_argument('data_dir', help="Dir containing all data needed for the challenge")
+    
+    args = parser.parse_args()
+    data_dir = args.data_dir
+    problem_statement, input, input_samples, output_samples = get_problemset(data_dir)
+    problem_content = problem_statement["contents"]
 
-# Make sure to incldue any images from the problem statement in the assets folder
-image_1 = encode_image('./assets/practice_problem/852013469652032.jpg')
-image_2 = encode_image('./assets/practice_problem/842253013944047.jpg')
+    # Make sure to incldue any images from the problem statement in the assets folder
+    images = []
+    for root, dirs, files in os.walk(data_dir):
+        for file in files:
+            if file.lower().endswith(".jpg"):
+                images.append({"type":"image_url","image_url":{"url":f"data:image/jpeg;base64,{encode_image(os.path.join(root, file))}"}})
 
-# Change this message if you select a different problem than Dim Sum Delivery
-problem_statement_message = HumanMessage(content=[
-            {"type": "text", "text": f"Can you please write code to solve the following problem? {problem_content}"},
-            {"type": "image_url","image_url": {"url": f"data:image/jpeg;base64,{image_1}"}},
-            {"type": "image_url","image_url": {"url": f"data:image/jpeg;base64,{image_2}"}}
-])
+    # Change this message if you select a different problem than Dim Sum Delivery
+    problem_statement_message = HumanMessage(content=[
+                {"type": "text", "text": f"Can you please write code to solve the following problem? {problem_content}"}]
+                #+images
+                )
 
-# Get the graph
-graph = get_graph_for_problem(ChatOpenAI(temperature=0, model="gpt-4o"), max_iterations = 3, flag ='reflect')
+    # Get the graph
+    graph = get_graph_for_problem(ChatOpenAI(temperature=0, model="gpt-4o"), max_iterations = 3, flag ='reflect')
+    
+    # Invoke graph, starting with 0 iterations
+    final_ans = graph.invoke(input={"messages":[problem_statement_message],"input_samples":input_samples,"output_samples":output_samples})['generation']
 
-# Invoke graph, starting with 0 iterations
-final_ans = graph.invoke(input={"messages":[problem_statement_message]})['generation']
+    '''
+    #See if the final answer is correct by checking the generated code against the test cases:
 
-'''
-See if the final answer is correct by checking the generated code against the test cases:
+    #This code will need to be changed if you use a problem other than the Dim Sum Delivery problem
+    '''
 
-This code will need to be changed if you use a problem other than the Dim Sum Delivery problem
-'''
+    # Get test cases
+    input_lines = input["location"].split("\n")[1:-1]
+    inputs = []
+    for i in range(len(input_lines)):
+        # NOTE: Not all problems have same test case format - make sure you append the right inputs (you may need to go through multiple lines!)
+        inputs.append([int(x) for x in input_lines[i].split(" ")])
 
-# Get test cases
-input_lines = input["contents"].split("\n")[1:]
-inputs = []
-for i in range(len(input_lines)):
-    # NOTE: Not all problems have same test case format - make sure you append the right inputs (you may need to go through multiple lines!)
-    inputs.append([int(x) for x in input_lines[i].split(" ")])
+    answers = []
+    existing_functions_before_exec = set(globals().keys())
+    exec(final_ans.imports + "\n" + final_ans.code)
+    existing_functions_after_exec = set(globals().keys())
+    # Make sure it is the correct function (length longer than 6 to avoid new variables in code)
+    new_function = [l for l in list(existing_functions_after_exec - existing_functions_before_exec) if l not in ['existing_functions_before_exec', 'Tuple'] and len(l) > 6][0]
+    for input in inputs:
+        # Make sure to check final_ans.code to see what function it is returning - in this case it is nim_sum_dim_sum
+        answers.append(globals()[new_function](*input))
 
-answers = []
-exec(final_ans.imports + "\n" + final_ans.code)
-for input in inputs:
-    # Make sure to check final_ans.code to see what function it is returning - in this case it is nim_sum_dim_sum
-    answers.append(nim_sum_dim_sum(*input))
-
-with open('output.txt', 'w') as file:
-    for i in range(len(answers)):
-        file.write(f"Case #{i+1}: "+answers[i]+"\n")
+    with open('output.txt', 'w') as file:
+        for i in range(len(answers)):
+            file.write(f"Case #{i+1}: "+answers[i]+"\n")
+    
--- langchain/utils/utils.py
+++ langchain/utils/utils.py
@@ -1,6 +1,10 @@
 import os
 import re
-directory_path = "./assets/practice_problem"  
+import base64
+
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
 
 def get_problem_files(directory):
     problem_output = {}
@@ -8,17 +12,19 @@ def get_problem_files(directory):
     for filename in os.listdir(directory):
         # Check if the file has .md extension
         if filename.endswith(".md"):
-            problem_output["problem"] = filename
+            problem_output["problem"] = directory+"/"+ filename
+        elif filename.endswith("samples.in"):
+            problem_output['input_samples'] = directory+"/"+filename
         elif filename.endswith(".in"):
-            problem_output["input"] = filename
-        elif filename.endswith(".out"):
-            problem_output["output"] = filename
+            problem_output["input"] = directory+"/"+filename
+        elif filename.endswith('samples.out'):
+            problem_output['output_samples'] = directory+"/"+filename
 
     return problem_output
 
-def parse_markdown(problemset_files):
+def parse_markdown(problemset_files,directory_path):
     md_file = problemset_files["problem"]
-    with open(f"{directory_path}/{md_file}", "r") as f:
+    with open(f"{md_file}", "r") as f:
         markdown_content = f.read()
         pattern = r"PHOTO_ID:\d+"
         matches = re.findall(pattern, markdown_content)
@@ -26,18 +32,28 @@ def parse_markdown(problemset_files):
             id = photo_id.replace("PHOTO_ID:", "")
             if len(id) > 0:
                 image_file = f"{directory_path}/{id}.jpg"
-                markdown_content = markdown_content.replace(photo_id, f"<img {image_file}>")
+                markdown_content = markdown_content.replace(photo_id, f"<img {encode_image(image_file)}>")
         return markdown_content
         
 def get_file_contents(filename):
     content = None
-    with open(f"{directory_path}/{filename}", "r") as f:
+    with open(f"{filename}", "r") as f:
         content = f.read()
     return content
 
-def get_problemset():
+def add_samples(markdown_content, input_sample_file, output_sample_file):
+    markdown_content+=f"\nInput samples: {{<txt {input_sample_file}>}}\nOutput samples: {{<txt {output_sample_file} >}}" 
+    return markdown_content
+
+def get_problemset(directory_path):
     problemset_files = get_problem_files(directory_path)
-    problem_statement = parse_markdown(problemset_files)
-    input = get_file_contents(problemset_files["input"])
+    problem_statement = parse_markdown(problemset_files,directory_path)
+
+    input_samples  = get_file_contents(problemset_files["input_samples"])
+    output_samples = get_file_contents( problemset_files['output_samples'])
+    problem_statement = add_samples( problem_statement, problemset_files['input_samples'],problemset_files['output_samples'] )
     return ({"type": "problem", "contents": problem_statement, "location": problemset_files["problem"]}, 
-            {"type": "input", "contents": input, "location": problemset_files["input"]})
+            {"type": "input",  "location": get_file_contents(problemset_files["input"])},
+            {"type": "input_samples", "contents": input_samples, "location":problemset_files['input_samples']},
+            {"type": "output_samples", "contents": output_samples, 'location':problemset_files['output_samples']},    
+            )
