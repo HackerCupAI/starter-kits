0b5f13caac0b1933f995c400792afe9eaa2319b3
Thomas Capelle <tcapelle@pm.me>
2024-08-02 09:49:49 +0200

    better download script with help

--- starter_solution/download.py
+++ starter_solution/download.py
@@ -5,8 +5,11 @@ from huggingface_hub import snapshot_download
 
 @dataclass
 class ScriptArgs:
-    year: int = 2023
-    dataset_folder: Path = Path("dataset")
+    """Download the dataset per year. Example usage:
+    python download.py --year 2023 --dataset_folder dataset
+    """
+    year: int = 2023 # year to download
+    dataset_folder: Path = Path("dataset") # folder to save the dataset
 
 if __name__ == "__main__":
     args = simple_parsing.parse(ScriptArgs)
