27d5395d210613edc9611f4bc2f47d5a5a797467
Thomas Capelle <tcapelle@pm.me>
2024-08-15 11:34:32 +0200

    return saved files

--- submit_first_solution/mini_lib/problem.py
+++ submit_first_solution/mini_lib/problem.py
@@ -58,19 +58,21 @@ class Problem:
     def get_output(self) -> str:
         return self.output_path.read_text()
 
-    def save_code(self, code):
+    def save_code(self, code: str, code_path: Optional[str] = None, outfile_name: Optional[str] = None):
         final_code = f"from pathlib import Path\ninput = Path('./{self.input_path.name}').read_text()\n\n"
         code_name = f"{self.name}_generated.py"
-        code_path = Path(self.folder_path) / code_name
+        code_path = Path(self.folder_path) / code_name if code_path is None else code_path
         final_code += code
-        outfile_name = f"{self.name}_generated.out"
-        final_code += f"\n\noutput = solve(input)\nPath('./{outfile_name}').write_text(output)\n"
+        outfile_name = f"./{self.name}_generated.out" if outfile_name is None else outfile_name
+        final_code += f"\n\noutput = solve(input)\nPath('{outfile_name}').write_text(output)\n"
         code_path.write_text(final_code)
+        return Path(code_path)
 
-    def save_output(self, output):
+    def save_output(self, output: str, outfile: Optional[str] = None):
         outfile_name = f"{self.name}_generated.out"
-        outfile = Path(self.folder_path) / outfile_name
+        outfile = Path(self.folder_path) / outfile_name if outfile is None else outfile
         outfile.write_text(output)
+        return Path(outfile)
 
     @classmethod
     def from_name(cls, name: str, folder_path: Path):
